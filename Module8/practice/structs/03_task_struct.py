4af58aa41396ab5c557f6505499df1ee6324f8ec
Matifiz <89151376+Matifiz@users.noreply.github.com>
2021-08-26 20:20:43 +0300

    Update 03_task_struct.py

--- Module8/practice/structs/03_task_struct.py
+++ Module8/practice/structs/03_task_struct.py
@@ -4,3 +4,18 @@
 # 1. Количество элементов списка не превышающие 10
 # 2. Сумму всех положительных элементов списка
 # 3. Среднее арифметическое всех четных элементов
+import random
+numbers = []
+n = 10
+for _ in range (10):
+    numbers.append(random.randint(-20, 20))
+print(numbers)
+
+larger_ten = [el for el in numbers if el >= 10]
+sum_positive = sum([el for el in numbers if el >0])
+positive = [el for el in numbers if el >0]
+mean_positive = sum(positive) / len(positive)
+print(larger_ten)
+print(sum_positive)
+print(positive)
+print(mean_positive)
