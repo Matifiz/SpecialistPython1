32b3efcd2c7050b6d19f516113eae65fc51058f1
Matifiz <89151376+Matifiz@users.noreply.github.com>
2021-08-28 22:36:06 +0300

    Update 02_task_result.py

--- Module10/practice/result/02_task_result.py
+++ Module10/practice/result/02_task_result.py
@@ -6,3 +6,16 @@
 # Формат выходных данных:
 # Выведите, во сколько раз отрезок AB больше, чем отрезок CD. Ответ введите с точностью до 6-ти знаков после запятой.
 
+a = float(input("Введите координаты точек: \nа:"))
+b = float(input("b:"))
+c = float(input("c:"))
+d = float(input("d:"))
+length_ab = abs(b - a)
+length_cd = abs(d - c)
+if length_ab > length_cd:
+    relation_times = length_ab / length_cd
+    print(f"Отрезок AB больше CD  в {relation_times:.6f} раз(а)")
+elif length_ab < length_cd:
+    relation_times = length_cd / length_ab
+    print(f"Отрезок AB меньше CD  в {relation_times:.6f} раз(а)")
+else: print("Отрезки равны")
